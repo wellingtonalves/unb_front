c8bf0b02e39b044b3035d6ec18f6ebef085f8676
user <fabiorochafg@gmail.com>
2020-09-24 21:21:04 -0300

    [19819] Corrigindo problemas

--- src/components/Layout/AppFooter.vue
+++ src/components/Layout/AppFooter.vue
@@ -1,99 +1,97 @@
 <template>
   <v-footer :absolute="false" class="justify-center">
 
-    <v-container>
-      <v-layout class="flex-column">
+    <v-layout class="flex-column">
 
-        <v-row>
-          <v-col cols="4">
-            <h2>EV.G | {{ $t('message.appFooter.portalUnico') }}</h2>
-            <p class="subtitulo">{{ $t('message.appFooter.solucaoUnica') }}.</p>
+      <v-row>
+        <v-col cols="4">
+          <h2>EV.G | {{ $t('message.appFooter.portalUnico') }}</h2>
+          <p class="subtitulo">{{ $t('message.appFooter.solucaoUnica') }}.</p>
 
-            <v-btn
-              v-for="icon in icons"
-              :key="icon.id"
-              class="mx-3 white--text"
-              icon
-              :href="icon.url"
-            >
-              <v-icon>{{ icon.icon }}</v-icon>
-            </v-btn>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon.id"
+            class="mx-3 white--text"
+            icon
+            :href="icon.url"
+          >
+            <v-icon>{{ icon.icon }}</v-icon>
+          </v-btn>
+          
+        </v-col>
+
+        <v-col cols="3">
+
+          <h3>{{ $t('message.appFooter.sobreEscola') }}</h3>
+          <v-list dense flat>
+            <v-list-item href="https://www.escolavirtual.gov.br/catalogo">
+              <v-list-item-content>
+                <v-list-item-title>{{ $t('message.appFooter.catalogoCursos') }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item href="https://www.escolavirtual.gov.br/programas">
+              <v-list-item-content>
+                <v-list-item-title>{{ $t('message.appFooter.catalogoProgramas') }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item href="https://www.escolavirtual.gov.br/conheca-a-escola">
+              <v-list-item-content>
+                <v-list-item-title>{{ $t('message.appFooter.conhecaEscola') }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item href="https://www.escolavirtual.gov.br/adesao-institucional">
+              <v-list-item-content>
+                <v-list-item-title>{{ $t('message.appFooter.adesaoInstitucional') }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item href="https://www.escolavirtual.gov.br/instituicoes-participantes">
+              <v-list-item-content>
+                <v-list-item-title>{{ $t('message.appFooter.instituicaoParticipante') }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item href="https://www.escolavirtual.gov.br/politica-privacidade">
+              <v-list-item-content>
+                <v-list-item-title>{{ $t('message.appFooter.politicaPrivacidade') }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
             
-          </v-col>
+          </v-list>
 
-          <v-col cols="3">
+        </v-col>
 
-            <h3>{{ $t('message.appFooter.sobreEscola') }}</h3>
-            <v-list dense flat>
-              <v-list-item href="https://www.escolavirtual.gov.br/catalogo">
-                <v-list-item-content>
-                  <v-list-item-title>{{ $t('message.appFooter.catalogoCursos') }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
+        <v-col cols="3">
 
-              <v-list-item href="https://www.escolavirtual.gov.br/programas">
-                <v-list-item-content>
-                  <v-list-item-title>{{ $t('message.appFooter.catalogoProgramas') }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
+          <h3>{{ $t('message.appFooter.utilidades') }}</h3>
+          <v-list dense flat>
+            <v-list-item href="https://www.escolavirtual.gov.br/documentos/validacao">
+              <v-list-item-content>
+                <v-list-item-title>{{ $t('message.appFooter.validacaoDocumentos') }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
 
-              <v-list-item href="https://www.escolavirtual.gov.br/conheca-a-escola">
-                <v-list-item-content>
-                  <v-list-item-title>{{ $t('message.appFooter.conhecaEscola') }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
+            <v-list-item href="https://www.escolavirtual.gov.br/perguntas-frequentes">
+              <v-list-item-content>
+                <v-list-item-title>{{ $t('message.appFooter.perguntasFrequentes') }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
 
-              <v-list-item href="https://www.escolavirtual.gov.br/adesao-institucional">
-                <v-list-item-content>
-                  <v-list-item-title>{{ $t('message.appFooter.adesaoInstitucional') }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
+        </v-col>
 
-              <v-list-item href="https://www.escolavirtual.gov.br/instituicoes-participantes">
-                <v-list-item-content>
-                  <v-list-item-title>{{ $t('message.appFooter.instituicaoParticipante') }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
+      </v-row>
 
-              <v-list-item href="https://www.escolavirtual.gov.br/politica-privacidade">
-                <v-list-item-content>
-                  <v-list-item-title>{{ $t('message.appFooter.politicaPrivacidade') }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-              
-            </v-list>
-
-          </v-col>
-
-          <v-col cols="3">
-
-            <h3>{{ $t('message.appFooter.utilidades') }}</h3>
-            <v-list dense flat>
-              <v-list-item href="https://www.escolavirtual.gov.br/documentos/validacao">
-                <v-list-item-content>
-                  <v-list-item-title>{{ $t('message.appFooter.validacaoDocumentos') }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-
-              <v-list-item href="https://www.escolavirtual.gov.br/perguntas-frequentes">
-                <v-list-item-content>
-                  <v-list-item-title>{{ $t('message.appFooter.perguntasFrequentes') }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-
-          </v-col>
-
-        </v-row>
-
-        <v-row>
-          <v-col cols="12">
-            <a href="http://www.enap.gov.br/" id="link-enap">{{ $t('message.appFooter.organizadoPor') }} – Enap</a>
-          </v-col>
-        </v-row>
-    
-      </v-layout>
-    </v-container>
+      <v-row>
+        <v-col cols="12">
+          <a href="http://www.enap.gov.br/" id="link-enap">{{ $t('message.appFooter.organizadoPor') }} – Enap</a>
+        </v-col>
+      </v-row>
+  
+    </v-layout>
 
   </v-footer>
 </template>
@@ -118,6 +116,8 @@
 <style scoped>
 footer.v-footer {
   background-color: var(--v-lightgrey-base);
+  margin-top: 32px;
+  padding: 6px 0;
 }
 footer .layout {
   border-top: 5px solid var(--v-primary-base);
