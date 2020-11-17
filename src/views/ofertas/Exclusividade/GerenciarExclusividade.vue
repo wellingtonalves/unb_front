2ab15fe31e0f46b2d442200657b7514f272b5d90
Alex Alexandre <alex.alexandre@redes.unb.br>
2020-11-16 23:00:55 -0300

    [UPD][20188] - implementando loading necessario no gerenciamento da oferta exclusiva

--- src/views/ofertas/Exclusividade/GerenciarExclusividade.vue
+++ src/views/ofertas/Exclusividade/GerenciarExclusividade.vue
@@ -1,112 +1,114 @@
 <template>
   <v-layout wrap>
     <card-default>
+
+      <form-skeleton :loading="loading" />
       
-      <h3>
-        Oferta: 
-        {{oferta.tx_nome_curso + " ( " + oferta.tx_nome_oferta + " )"}}
-      </h3>
-      
-      <v-row>
-        <v-col>
-          <v-btn outlined color="primary" v-show="permission('EXCLUSIVIDADE_EDITAR')"
-                 @click="$router.push(`/ofertas/${oferta.id_oferta}/gerenciar-exclusividade/${oferta.exclusividade.id_exclusividade_oferta}/create`)">
-            Adicionar Regra
+      <div v-show="!loading">
+        <h3>
+          Oferta:
+          {{oferta.tx_nome_curso + " ( " + oferta.tx_nome_oferta + " )"}}
+        </h3>
+
+        <v-row>
+          <v-col>
+            <v-btn outlined color="primary" v-show="permission('EXCLUSIVIDADE_EDITAR')"
+                   @click="$router.push(`/ofertas/${oferta.id_oferta}/gerenciar-exclusividade/${oferta.exclusividade.id_exclusividade_oferta}/create`)">
+              Adicionar Regra
+            </v-btn>
+          </v-col>
+
+          <v-col>
+            <v-btn outlined color="accent" v-show="permission('EXCLUSIVIDADE_EDITAR')"
+                   @click="$router.push(`/ofertas/${oferta.id_oferta}/exclusividade/${oferta.exclusividade.id_exclusividade_oferta}/edit`)">
+              Editar
+            </v-btn>
+          </v-col>
+
+          <v-col>
+            <v-btn outlined color="error" v-show="permission('EXCLUSIVIDADE_EXCLUIR')"
+                   @click="excluir()">
+              Remover exclusividade
+            </v-btn>
+          </v-col>
+
+        </v-row>
+
+
+        <v-row class="flex-basis-100">
+          <v-col cols="12">
+            <v-card>
+
+              <v-data-table
+                :headers="headersValorExclusividade"
+                :items="valorExclusividade"
+                :search="searchValorExclusividade"
+                item-key="id_valor_exclusividade_oferta"
+                sort-by="valor_exclusividade"
+                class="elevation-1"
+                no-data-text="Nenhum registro encontrado."
+                no-results-text="Nenhum registro encontrado.">
+
+                <template v-slot:top>
+                  <v-toolbar flat>
+                    <v-toolbar-title>Valores cadastrados</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-text-field
+                      v-model="searchValorExclusividade"
+                      append-icon="mdi-magnify"
+                      label="Procurar"
+                      single-line
+                      hide-details
+                    ></v-text-field>
+                  </v-toolbar>
+                </template>
+
+                <template v-slot:item.action="{ item }">
+
+                  <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                      <v-btn v-show="permission('VALOR_EXCLUSIVIDADE_EXCLUIR')" small color="error" icon @click="confirmarExcluirValorExclusividade(item)"
+                             v-on="on">
+                        <v-icon>mdi-delete</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Excluir</span>
+                  </v-tooltip>
+
+                </template>
+
+              </v-data-table>
+
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-btn class="mr-4" @click="$router.push('/ofertas')">
+            <v-icon class="mr-2">mdi-backup-restore</v-icon>
+            Voltar
           </v-btn>
-        </v-col>
+        </v-row>
 
-        <v-col>
-          <v-btn outlined color="accent" v-show="permission('EXCLUSIVIDADE_EDITAR')" 
-                 @click="$router.push(`/ofertas/${oferta.id_oferta}/exclusividade/${oferta.exclusividade.id_exclusividade_oferta}/edit`)">
-            Editar
-          </v-btn>
-        </v-col>
+        <v-row justify="center">
+          <v-dialog v-model="dialogDelete" persistent max-width="500">
+            <v-card>
+              <v-card-title class="headline">Atenção!</v-card-title>
+              <v-card-text>Deseja remover a exclusividade desta oferta ?</v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="error" text @click="dialogDelete = false">Cancelar</v-btn>
+                <v-btn color="primary" text @click="excluirItem(oferta.exclusividade.id_exclusividade_oferta)">Confirmar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-row>
 
-        <v-col>
-          <v-btn outlined color="error" v-show="permission('EXCLUSIVIDADE_EXCLUIR')" 
-                 @click="excluir()">
-            Remover exclusividade
-          </v-btn>
-        </v-col>
-        
-      </v-row>
-
-
-      <v-row class="flex-basis-100">
-        <v-col cols="12">
-          <v-card>
-
-            <v-data-table
-              :headers="headersValorExclusividade"
-              :items="valorExclusividade"
-              :loading="loading"
-              :search="searchValorExclusividade"
-              item-key="id_valor_exclusividade_oferta"
-              sort-by="valor_exclusividade"
-              class="elevation-1"
-              no-data-text="Nenhum registro encontrado."
-              no-results-text="Nenhum registro encontrado."
-              loading-text="Aguarde, estamos carregando os dados.">
-
-              <template v-slot:top>
-                <v-toolbar flat>
-                  <v-toolbar-title>Valores cadastrados</v-toolbar-title>
-                  <v-spacer></v-spacer>
-                  <v-text-field
-                    v-model="searchValorExclusividade"
-                    append-icon="mdi-magnify"
-                    label="Procurar"
-                    single-line
-                    hide-details
-                  ></v-text-field>
-                </v-toolbar>
-              </template>
-
-              <template v-slot:item.action="{ item }">
-
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
-                    <v-btn v-show="permission('VALOR_EXCLUSIVIDADE_EXCLUIR')" small color="error" icon @click="confirmarExcluirValorExclusividade(item)"
-                           v-on="on">
-                      <v-icon>mdi-delete</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Excluir</span>
-                </v-tooltip>
-
-              </template>
-
-            </v-data-table>
-
-          </v-card>
-        </v-col>
-      </v-row>
-      
-      <v-row>
-        <v-btn class="mr-4" @click="$router.push('/ofertas')">
-          <v-icon class="mr-2">mdi-backup-restore</v-icon>
-          Voltar
-        </v-btn>
-      </v-row>
-
-      <v-row justify="center">
-        <v-dialog v-model="dialogDelete" persistent max-width="500">
-          <v-card>
-            <v-card-title class="headline">Atenção!</v-card-title>
-            <v-card-text>Deseja remover a exclusividade desta oferta ?</v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="error" text @click="dialogDelete = false">Cancelar</v-btn>
-              <v-btn color="primary" text @click="excluirItem(oferta.exclusividade.id_exclusividade_oferta)">Confirmar</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-row>
-
-      <dialog-delete-component 
-        :text="dialogValorExclusividadeDeleteData.valor_exclusividade" 
-        v-model="dialogValorExclusividadeDelete" 
-        @excluir="excluirValorExclusividade(dialogValorExclusividadeDeleteData.id_valor_exclusividade_oferta)" />
+        <dialog-delete-component
+          :text="dialogValorExclusividadeDeleteData.valor_exclusividade"
+          v-model="dialogValorExclusividadeDelete"
+          @excluir="excluirValorExclusividade(dialogValorExclusividadeDeleteData.id_valor_exclusividade_oferta)" />
+      </div>
       
     </card-default>
   </v-layout>
@@ -120,7 +122,7 @@
     name: "GerenciarExclusividade",
     data: () => ({
       data: [],
-      loading: false,
+      loading: true,
       oferta: {},
       valorExclusividade: [],
       headersValorExclusividade: [
@@ -146,6 +148,7 @@
         const response = await get(`ofertas/${this.$route.params.id}`);
         this.oferta = response.data.data;
         this.valorExclusividade = response.data.data.exclusividade.valor_exclusividade;
+        this.loading = false;
       },
       excluir() {
         this.dialogDelete = true;
--- src/views/ofertas/Exclusividade/GerenciarExclusividadeCreate.vue
+++ src/views/ofertas/Exclusividade/GerenciarExclusividadeCreate.vue
@@ -2,62 +2,66 @@
   <v-layout wrap>
     <card-default>
 
-      <h3>
-        Oferta:
-        {{oferta.tx_nome_curso + " ( " + oferta.tx_nome_oferta + " )"}}
-      </h3>
+      <form-skeleton :loading="loadingOferta" />
 
-      <v-form lazy-validation ref="form">
-        <v-row>
-          <v-text-field
-            outlined
-            label="Valor"
-            required
-            v-model="valor_exclusividade"
-          />
-        </v-row>
-        
-        <v-row>
-          <v-file-input label="Carregar arquivo"  outlined dense v-model="anexo"/> 
-        </v-row>
-        
-      </v-form>
-      
-      <div>
-        <v-alert text color="warning">
+      <div v-show="!loadingOferta">
+        <h3>
+          Oferta:
+          {{oferta.tx_nome_curso + " ( " + oferta.tx_nome_oferta + " )"}}
+        </h3>
+
+        <v-form lazy-validation ref="form">
+          <v-row>
+            <v-text-field
+              outlined
+              label="Valor"
+              required
+              v-model="valor_exclusividade"
+            />
+          </v-row>
+
+          <v-row>
+            <v-file-input label="Carregar arquivo"  outlined dense v-model="anexo"/>
+          </v-row>
+
+        </v-form>
+
+        <div>
+          <v-alert text color="warning">
                 <span>
                     <strong>Atenção!</strong>
                     Caso queira adicionar um arquivo com os critérios, siga o seguinte padrão:
                 </span>
-        </v-alert>
+          </v-alert>
 
-        <p>Formato do Arquivo: <span style="color: red">csv</span></p>
-        <p>Separado por: <span style="color: red">',' (Vírgula)</span></p>
-        <p>Primeira Célula <span style="color: red">(OBRIGATORIAMENTE)</span>, com o texto:<span style="color: red"> 'valores'</span></p>
-        <p>Exemplo:</p>
-        
-        <v-list>
-          <v-list-item v-for="(item, i) in ValoresExemplosEmail" :key="i">
-            <v-list-item-content>
-              <v-list-item-title v-text="item.text" />
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
+          <p>Formato do Arquivo: <span style="color: red">csv</span></p>
+          <p>Separado por: <span style="color: red">',' (Vírgula)</span></p>
+          <p>Primeira Célula <span style="color: red">(OBRIGATORIAMENTE)</span>, com o texto:<span style="color: red"> 'valores'</span></p>
+          <p>Exemplo:</p>
 
-        <p>Baixe o arquivo de Exemplo: aqui</p>
+          <v-list>
+            <v-list-item v-for="(item, i) in ValoresExemplosEmail" :key="i">
+              <v-list-item-content>
+                <v-list-item-title v-text="item.text" />
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <p>Baixe o arquivo de Exemplo: aqui</p>
+        </div>
+
+        <v-row class="mt-5" justify="center">
+          <v-btn class="mx-2" @click="$router.go(-1)">
+            <v-icon class="mr-2">mdi-backup-restore</v-icon>
+            Voltar
+          </v-btn>
+
+          <v-btn class="mx-2" color="primary" :loading="loading" @click="save()">
+            <v-icon class="mr-2">mdi-content-save</v-icon>
+            Salvar
+          </v-btn>
+        </v-row>
       </div>
-      
-      <v-row class="mt-5" justify="center">
-        <v-btn class="mx-2" @click="$router.go(-1)">
-          <v-icon class="mr-2">mdi-backup-restore</v-icon>
-          Voltar
-        </v-btn>
-
-        <v-btn class="mx-2" color="primary" :loading="loading" @click="save()">
-          <v-icon class="mr-2">mdi-content-save</v-icon>
-          Salvar
-        </v-btn>
-      </v-row>
 
       
     </card-default>
@@ -70,6 +74,7 @@
     name: "GerenciarExclusividadeCreate",
     data: () => ({
       data: {},
+      loadingOferta: true,
       loading: false,
       oferta: {},
       valor_exclusividade: '',
@@ -91,6 +96,7 @@
       async getOferta() {
         const response = await get(`ofertas/${this.$route.params.id}`);
         this.oferta = response.data.data;
+        this.loadingOferta = false;
       },
       async save() {
         this.loading = true;
