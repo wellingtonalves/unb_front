8ec40a1330e2651545650dbd23d0c2f27872430e
user <fabiorochafg@gmail.com>
2020-11-30 11:39:41 -0300

    Padronizando layout de cards de cursos e programas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
 
     <h2 class="featured">Cursos <strong>em Destaque</strong></h2>
 
-    <v-layout class="course-cards flex-wrap justify-center">
+    <v-layout class="cards course-cards flex-wrap justify-center">
 
       <v-card class="d-flex">
         <div>
@@ -18,18 +18,18 @@
             <dt>Carga Horária:</dt>
             <dd>20h</dd>
         </dl>
-        <div class="action-bar d-flex">
-            <v-btn tile outlined color="primary" class="ma-2" href="https://www.escolavirtual.gov.br/curso/258/">
-              <v-icon>mdi-information</v-icon>
-              <span class="d-sr-only">Saiba mais</span>
-            </v-btn>
-            <v-btn tile color="contrast" class="ma-2" href="https://www.escolavirtual.gov.br/secretaria/inscricao/5437">
-              Inscreva-se
-              <v-icon right>
-                mdi-menu-right
-              </v-icon>
-            </v-btn>
-        </div>
+        <v-card-actions>
+          <v-btn tile outlined color="primary" class="ma-2" href="https://www.escolavirtual.gov.br/curso/258/">
+            <v-icon>mdi-information</v-icon>
+            <span class="d-sr-only">Saiba mais</span>
+          </v-btn>
+          <v-btn tile color="contrast" class="ma-2 flex-grow-1" href="https://www.escolavirtual.gov.br/secretaria/inscricao/5437">
+            Inscreva-se
+            <v-icon right>
+              mdi-menu-right
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
       </v-card>
 
       <v-card class="d-flex">
@@ -45,18 +45,18 @@
             <dt>Carga Horária:</dt>
             <dd>20h</dd>
         </dl>
-        <div class="action-bar d-flex">
-            <v-btn tile outlined color="primary" class="ma-2" href="https://www.escolavirtual.gov.br/curso/258/">
-              <v-icon>mdi-information</v-icon>
-              <span class="d-sr-only">Saiba mais</span>
-            </v-btn>
-            <v-btn tile color="contrast" class="ma-2" href="https://www.escolavirtual.gov.br/secretaria/inscricao/5437">
-              Inscreva-se
-              <v-icon right>
-                mdi-menu-right
-              </v-icon>
-            </v-btn>
-        </div>
+        <v-card-actions>
+          <v-btn tile outlined color="primary" class="ma-2" href="https://www.escolavirtual.gov.br/curso/258/">
+            <v-icon>mdi-information</v-icon>
+            <span class="d-sr-only">Saiba mais</span>
+          </v-btn>
+          <v-btn tile color="contrast" class="ma-2 flex-grow-1" href="https://www.escolavirtual.gov.br/secretaria/inscricao/5437">
+            Inscreva-se
+            <v-icon right>
+              mdi-menu-right
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
       </v-card>
 
       <v-card class="d-flex">
@@ -72,18 +72,150 @@
             <dt>Carga Horária:</dt>
             <dd>20h</dd>
         </dl>
-        <div class="action-bar d-flex">
-            <v-btn tile outlined color="primary" class="ma-2" href="https://www.escolavirtual.gov.br/curso/258/">
-              <v-icon>mdi-information</v-icon>
-              <span class="d-sr-only">Saiba mais</span>
-            </v-btn>
-            <v-btn tile color="contrast" class="ma-2" href="https://www.escolavirtual.gov.br/secretaria/inscricao/5437">
-              Inscreva-se
-              <v-icon right>
-                mdi-menu-right
-              </v-icon>
-            </v-btn>
+        <v-card-actions>
+          <v-btn tile outlined color="primary" class="ma-2" href="https://www.escolavirtual.gov.br/curso/258/">
+            <v-icon>mdi-information</v-icon>
+            <span class="d-sr-only">Saiba mais</span>
+          </v-btn>
+          <v-btn tile color="contrast" class="ma-2 flex-grow-1" href="https://www.escolavirtual.gov.br/secretaria/inscricao/5437">
+            Inscreva-se
+            <v-icon right>
+              mdi-menu-right
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+
+      <v-card class="d-flex">
+        <div>
+          <v-img src="https://cdn.evg.gov.br/cursos/258_EVG/banner.svg" alt="Logo do Curso Inovação Social para o Aperfeiçoamento de Políticas Públicas"></v-img>
+          <p class="nome-tematica" data-paleta-bg="9" title="Gestão de Políticas Públicas">Gestão de Políticas Públicas</p>
+          <p class="nome-oferta">Curso Aberto</p>
+          <h3 class="v-card__title"><a href="https://www.escolavirtual.gov.br/curso/258">Inovação Social para o Aperfeiçoamento de Políticas Públicas</a></h3>
         </div>
+        <dl>
+             <dt>Conteudista:</dt>
+             <dd>TCU</dd>
+            <dt>Carga Horária:</dt>
+            <dd>20h</dd>
+        </dl>
+        <v-card-actions>
+          <v-btn tile outlined color="primary" class="ma-2" href="https://www.escolavirtual.gov.br/curso/258/">
+            <v-icon>mdi-information</v-icon>
+            <span class="d-sr-only">Saiba mais</span>
+          </v-btn>
+          <v-btn tile color="contrast" class="ma-2 flex-grow-1" href="https://www.escolavirtual.gov.br/secretaria/inscricao/5437">
+            Inscreva-se
+            <v-icon right>
+              mdi-menu-right
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+
+    </v-layout>
+
+    <h2 class="featured mt-16">Programas <strong>em Destaque</strong></h2>
+
+    <v-layout class="cards course-cards flex-wrap justify-center">
+
+      <v-card class="d-flex">
+        <v-img src="https://cdn.evg.gov.br/programas/imagem_programa_2.png" alt="Logo do Programa Certificação em Ouvidoria"></v-img>
+        <div>
+          <h3 class="v-card__title"><a href="#">Certificação em Ouvidoria - 160 horas</a></h3>
+          <dl>
+            <dt>Carga Horária:</dt>
+            <dd>160h</dd>
+          </dl>
+        </div>
+        <v-card-actions>
+          <v-btn tile outlined color="primary" class="ma-2" @click="reveal = true">
+            <v-icon>mdi-information</v-icon>
+            <span class="d-sr-only">Saiba mais</span>
+          </v-btn>
+          <v-btn tile color="contrast" class="ma-2 flex-grow-1" href="https://www.escolavirtual.gov.br/secretaria/inscricao/5437">
+            Acesse
+            <v-icon right>
+              mdi-menu-right
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+          <v-card v-if="reveal" class="transition-fast-in-fast-out v-card--reveal">
+            <p class="v-card__text">A CERTIFICAÇÃO EM OUVIDORIA é concedida pela Ouvidoria-Geral da União (OGU), da Controladoria-Geral da União (CGU), em parceria com Escola Nacional de Administração Pública (Enap), aos alunos que concluírem oito cursos oferecidos em modalidade a distância, com carga horária total de 160 horas, realizadas dentro do mesmo ano. Para mais informações sobre o PROFOCO e a CERTIFICAÇÃO EM OUVIDORIA, aces...o (CGU), em parceria com Escola Nacional de Administração Pública (Enap), aos alunos que concluírem oito cursos oferecidos em modalidade a distância, com carga horária total de 160 horas, realizadas dentro do mesmo ano. Para mais informações sobre o PROFOCO e a CERTIFICAÇÃO EM OUVIDORIA, aces...</p>
+            <v-card-actions>
+              <v-btn text @click="reveal = false">
+                Fechar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-expand-transition>
+      </v-card>
+
+      <v-card class="d-flex">
+        <v-img src="https://cdn.evg.gov.br/programas/imagem_programa_2.png" alt="Logo do Programa Certificação em Ouvidoria"></v-img>
+        <div>
+          <h3 class="v-card__title"><a href="#">Certificação em Ouvidoria - 160 horas</a></h3>
+          <dl>
+            <dt>Carga Horária:</dt>
+            <dd>160h</dd>
+          </dl>
+        </div>
+        <v-card-actions>
+          <v-btn tile outlined color="primary" class="ma-2" @click="reveal = true">
+            <v-icon>mdi-information</v-icon>
+            <span class="d-sr-only">Saiba mais</span>
+          </v-btn>
+          <v-btn tile color="contrast" class="ma-2 flex-grow-1" href="https://www.escolavirtual.gov.br/secretaria/inscricao/5437">
+            Acesse
+            <v-icon right>
+              mdi-menu-right
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+          <v-card v-if="reveal" class="transition-fast-in-fast-out v-card--reveal">
+            <p class="v-card__text">A CERTIFICAÇÃO EM OUVIDORIA é concedida pela Ouvidoria-Geral da União (OGU), da Controladoria-Geral da União (CGU), em parceria com Escola Nacional de Administração Pública (Enap), aos alunos que concluírem oito cursos oferecidos em modalidade a distância, com carga horária total de 160 horas, realizadas dentro do mesmo ano. Para mais informações sobre o PROFOCO e a CERTIFICAÇÃO EM OUVIDORIA, aces...o (CGU), em parceria com Escola Nacional de Administração Pública (Enap), aos alunos que concluírem oito cursos oferecidos em modalidade a distância, com carga horária total de 160 horas, realizadas dentro do mesmo ano. Para mais informações sobre o PROFOCO e a CERTIFICAÇÃO EM OUVIDORIA, aces...</p>
+            <v-card-actions>
+              <v-btn text @click="reveal = false">
+                Fechar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-expand-transition>
+      </v-card>
+
+      <v-card class="d-flex">
+        <v-img src="https://cdn.evg.gov.br/programas/imagem_programa_2.png" alt="Logo do Programa Certificação em Ouvidoria"></v-img>
+        <div>
+          <h3 class="v-card__title"><a href="#">Certificação em Ouvidoria - 160 horas</a></h3>
+          <dl>
+            <dt>Carga Horária:</dt>
+            <dd>160h</dd>
+          </dl>
+        </div>
+        <v-card-actions>
+          <v-btn tile outlined color="primary" class="ma-2" @click="reveal = true">
+            <v-icon>mdi-information</v-icon>
+            <span class="d-sr-only">Saiba mais</span>
+          </v-btn>
+          <v-btn tile color="contrast" class="ma-2 flex-grow-1" href="https://www.escolavirtual.gov.br/secretaria/inscricao/5437">
+            Acesse
+            <v-icon right>
+              mdi-menu-right
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+          <v-card v-if="reveal" class="transition-fast-in-fast-out v-card--reveal">
+            <p class="v-card__text">A CERTIFICAÇÃO EM OUVIDORIA é concedida pela Ouvidoria-Geral da União (OGU), da Controladoria-Geral da União (CGU), em parceria com Escola Nacional de Administração Pública (Enap), aos alunos que concluírem oito cursos oferecidos em modalidade a distância, com carga horária total de 160 horas, realizadas dentro do mesmo ano. Para mais informações sobre o PROFOCO e a CERTIFICAÇÃO EM OUVIDORIA, aces...o (CGU), em parceria com Escola Nacional de Administração Pública (Enap), aos alunos que concluírem oito cursos oferecidos em modalidade a distância, com carga horária total de 160 horas, realizadas dentro do mesmo ano. Para mais informações sobre o PROFOCO e a CERTIFICAÇÃO EM OUVIDORIA, aces...</p>
+            <v-card-actions>
+              <v-btn text @click="reveal = false">
+                Fechar
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-expand-transition>
       </v-card>
 
     </v-layout>
@@ -123,16 +255,13 @@
 
 export default {
   name: 'Home',
+  data: () => ({
+    reveal: false,
+  }),
   components: {}
 }
 </script>
 
 <style scoped>
-.v-card__actions {
-    padding: 0 32px 32px;
-}
-.v-card__actions .v-icon {
-  font-size: 16px;
-  margin-right: 8px;
-}
+
 </style>
