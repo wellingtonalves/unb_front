cc16aa6c2e99a4863b1fc8352e5c88287deb84aa
João Marcos <joaomarcos.src@gmail.com>
2021-01-07 15:49:49 -0300

    [UPD][21152] -Implementando carrossel em cursos em destaque da página inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,9 @@
     <v-layout id="featured">
       <div class="centered">
         <h2 class="white--text text-center">Escola Virtual.Gov – EV.G</h2>
-        <h3 class="font-weight-light white--text text-center">
-          Portal Único de Governo para a oferta de capacitação a distância.
-        </h3>
+        <h3
+          class="font-weight-light white--text text-center"
+        >Portal Único de Governo para a oferta de capacitação a distância.</h3>
         <p class="font-weight-light white--text text-center">
           Cursos on-line e gratuitos de várias áreas de conhecimento para o
           desenvolvimento da Administração Pública e da Sociedade.
@@ -22,7 +22,28 @@
             <strong>em Destaque</strong>
           </h2>
 
-          <list-cursos-cards v-if="cursos.data" :curso-data="cursos.data" />
+          <v-carousel hide-delimiters v-model="cursoModel" interval="6000" cycle>
+            <v-carousel-item v-for="index in arrCount" :key="index">
+              <v-responsive dark>
+                <v-row no-gutters justify="space-around">
+                  <v-col class="d-flex justify-center">
+                    <div class="desktop-scroll scroll">
+                      <div class="content-desktop">
+                        <v-row>
+                          <v-col>
+                            <list-cursos-cards
+                              v-if="carouselItems.length"
+                              :curso-data="carouselItems[cursoModel]"
+                            />
+                          </v-col>
+                        </v-row>
+                      </div>
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-responsive>
+            </v-carousel-item>
+          </v-carousel>
 
           <v-flex text-center class="mt-6">
             <v-btn large outlined color="primary" @click="$router.push('/catalogo-cursos')">
@@ -42,18 +63,10 @@
             <strong>em Destaque</strong>
           </h2>
 
-          <list-programas-cards
-            v-if="programas.data"
-            :programa-data="programas.data"
-          />
+          <list-programas-cards v-if="programas.data" :programa-data="programas.data" />
 
           <v-flex text-center class="mt-6">
-            <v-btn
-              large
-              outlined
-              color="primary"
-              @click="$router.push('/catalogo-programas')"
-            >
+            <v-btn large outlined color="primary" @click="$router.push('/catalogo-programas')">
               Ver todos os programas
               <v-icon right>mdi-menu-right</v-icon>
             </v-btn>
@@ -75,10 +88,7 @@
             <strong>Catálogo de Cursos</strong> ou pela sugestão da página
             inicial.
           </v-card-title>
-          <v-img
-            src="../assets/img/como-funciona-01.svg"
-            alt="Passo 01"
-          ></v-img>
+          <v-img src="../assets/img/como-funciona-01.svg" alt="Passo 01"></v-img>
         </v-card>
 
         <v-card class="d-flex">
@@ -86,32 +96,21 @@
             Leia a descrição do curso e acesse o link
             <strong>Inscreva-se</strong>.
           </v-card-title>
-          <v-img
-            src="../assets/img/como-funciona-02.svg"
-            alt="Passo 02"
-          ></v-img>
+          <v-img src="../assets/img/como-funciona-02.svg" alt="Passo 02"></v-img>
         </v-card>
 
         <v-card class="d-flex">
           <v-card-title>Acompanhe as instruções de cadastro e...</v-card-title>
-          <v-img
-            src="../assets/img/como-funciona-03.svg"
-            alt="Passo 03"
-          ></v-img>
+          <v-img src="../assets/img/como-funciona-03.svg" alt="Passo 03"></v-img>
         </v-card>
 
         <v-card class="d-flex">
-          <v-card-title>
-            ... pronto, você já está dentro do ambiente de curso.
-          </v-card-title>
+          <v-card-title>... pronto, você já está dentro do ambiente de curso.</v-card-title>
           <v-card-text>
             Lá você poderá ver também todos os cursos que você virá a se
             inscrever.
           </v-card-text>
-          <v-img
-            src="../assets/img/como-funciona-04.svg"
-            alt="Passo 04"
-          ></v-img>
+          <v-img src="../assets/img/como-funciona-04.svg" alt="Passo 04"></v-img>
         </v-card>
       </v-layout>
     </v-container>
@@ -124,17 +123,33 @@ import {get} from '@/services/abstract.service';
 export default {
   name: 'Home',
   data: () => ({
-    cursos: {},
     programas: {},
+    carouselItems: [],
+    arrCount: [],
+    cursoModel: 0,
   }),
   created() {
     this.getCursosDestaque();
     this.getProgramasDestaque();
   },
   methods: {
+    countSlides(arr) {
+      this.arrCount = Array.from({length: arr.length}, (_, i) => i + 1);
+    },
+    chunkCarousel(array, chunkSize) {
+      let chunkCount = Math.ceil(array.length / chunkSize);
+      let chunks = new Array(chunkCount);
+      for (let i = 0, j = 0, k = chunkSize; i < chunkCount; ++i) {
+        chunks[i] = array.slice(j, k);
+        j = k;
+        k += chunkSize;
+      }
+      return chunks;
+    },
     async getCursosDestaque() {
       const response = await get('/curso?search=bl_destaque_curso:1');
-      this.cursos = response.data;
+      this.carouselItems = this.chunkCarousel(response.data.data, 4);
+      this.countSlides(this.carouselItems);
     },
     async getProgramasDestaque() {
       const response = await get('/programas?search=bl_programa_destaque:1');
