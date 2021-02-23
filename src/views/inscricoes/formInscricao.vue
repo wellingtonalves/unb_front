1ea2d6d4863d7853958035a7aaf7281f3950faed
Alex Alexandre <alex.alexandre@redes.unb.br>
2021-02-22 21:05:30 -0300

    [21149][UPD] - implementando regras para ao servidor publico do poder executivo

--- src/views/inscricoes/formInscricao.vue
+++ src/views/inscricoes/formInscricao.vue
@@ -14,44 +14,53 @@
 
             <v-col cols="12" sm="6">
               <v-select v-model="dataResponse.tp_poder_execut_legisl_judic" :error-messages="errorData.tp_poder_execut_legisl_judic"
-                        :items="tpPoderExectLegislJudic" outlined label="De qual poder?" item-text="label" item-value="value" />
+                        :items="tpPoderExectLegislJudic" outlined label="De qual poder?" item-text="label" item-value="value" 
+                        v-show="dataResponse.tp_servidor_militar_cidadao == 'S'" @change="tratarServidorPublico()" />
             </v-col>
 
             <v-col cols="12" sm="4">
               <v-select v-model="dataResponse.tp_esfera_servidor_militar" :error-messages="errorData.tp_esfera_servidor_militar"
-                        :items="tpEsferaServidorMilitar" outlined label="De qual esfera?" item-text="label" item-value="value" />
+                        :items="tpEsferaServidorMilitar" outlined label="De qual esfera?" item-text="label" item-value="value"
+                        v-show="showFields.tp_esfera_servidor_militar" @change="tratarEsferaPoderExecutivoServidor()"/>
+<!--                        v-show="dataResponse.tp_servidor_militar_cidadao == 'M' || dataResponse.tp_servidor_militar_cidadao == 'E'" />-->
             </v-col>
 
             <v-col cols="12" sm="4">
-              <v-select v-model="dataResponse.sg_uf_servidor_estadual" :error-messages="errorData.sg_uf_servidor_estadual"
-                        :items="sgUfServidorEstadual" outlined label="De qual UF?" item-text="tx_nome_uf" item-value="sg_uf" @change="getMunicipio()"/>
+              <v-autocomplete v-model="dataResponse.sg_uf_servidor_estadual" :error-messages="errorData.sg_uf_servidor_estadual" :loading="loadingUf"
+                        :items="sgUfServidorEstadual" outlined label="De qual UF?" item-text="tx_nome_uf" item-value="sg_uf" 
+                        v-show="showFields.sg_uf_servidor_estadual" @change="verificarRegrasParaUf()"/>
             </v-col>
 
             <v-col cols="12" sm="4">
-              <v-select v-model="dataResponse.id_municipio_servidor_municipal" :error-messages="errorData.id_municipio_servidor_municipal"
-                        :items="idMunicipioServidorMunicipal" outlined label="De qual Município?" item-text="tx_nome_municipio" item-value="id_municipio" />
+              <v-autocomplete v-model="dataResponse.id_municipio_servidor_municipal" :error-messages="errorData.id_municipio_servidor_municipal" :loading="loadingMunicipio"
+                        :items="idMunicipioServidorMunicipal" outlined label="De qual Município?" item-text="tx_nome_municipio" item-value="id_municipio" 
+                        v-show="showFields.id_municipio_servidor_municipal" />
             </v-col>
 
             <v-col cols="12">
-              <v-select v-model="dataResponse.id_orgao_servidor" :error-messages="errorData.id_orgao_servidor"
-                          :items="idOrgaoServidor" outlined label="De qual órgão?" item-text="tx_nome_orgao" item-value="id_orgao"
-                          hint="As informações deste campo são disponibilizadas pelo Sistema de Informações Organizacionais do Governo Federal. Caso não encontre a informação que deseje na relação, utilize o órgão imediatamente superior." persistent-hint />
+              <v-autocomplete v-model="dataResponse.id_orgao_servidor" :error-messages="errorData.id_orgao_servidor" :loading="loadingOrgaos"
+                        :items="idOrgaoServidor" outlined label="De qual órgão?" item-text="tx_nome_orgao" item-value="id_orgao" 
+                        hint="As informações deste campo são disponibilizadas pelo Sistema de Informações Organizacionais do Governo Federal. Caso não encontre a informação que deseje na relação, utilize o órgão imediatamente superior." persistent-hint 
+                        v-show="showFields.id_orgao_servidor" />
             </v-col>
 
             <v-col cols="12" sm="6">
               <v-select v-model="dataResponse.tp_ocupa_cargo_funcao" :error-messages="errorData.tp_ocupa_cargo_funcao"
-                        :items="tpOcupaCargoFuncao" outlined label="Ocupa função comissionada?" item-text="label" item-value="value" />
+                        :items="tpOcupaCargoFuncao" outlined label="Ocupa função comissionada?" item-text="label" item-value="value" 
+                        v-show="dataResponse.tp_servidor_militar_cidadao == 'S'" />
             </v-col>
 
             <v-col cols="12" sm="6">
 <!--              TODO -- essa lista deverá ser filtrada e não está de acordo com os dados de produção-->
               <v-select v-model="dataResponse.tp_empregado_terceiro_setor" :error-messages="errorData.tp_empregado_terceiro_setor"
-                        :items="idOrgaoServidor" outlined label="De qual empresa?" item-text="tx_nome_orgao" item-value="id_orgao" />
+                        :items="idOrgaoServidor" outlined label="De qual empresa?" item-text="tx_nome_orgao" item-value="id_orgao"
+                        v-show="dataResponse.tp_servidor_militar_cidadao == 'ETR'" />
             </v-col>
 
             <v-col cols="12">
               <v-select v-model="dataResponse.tp_sem_vinculo" :error-messages="errorData.tp_sem_vinculo"
-                        :items="tpSemVinculo" outlined label="Selecione sua situação" item-text="label" item-value="value" />
+                        :items="tpSemVinculo" outlined label="Selecione sua situação" item-text="label" item-value="value"
+                                      v-show="dataResponse.tp_servidor_militar_cidadao == 'SV'" />
             </v-col>
             
           </v-row>
@@ -63,23 +72,23 @@
 
             <v-col cols="12">
               <v-switch
-                v-model="resideNoBrasil"
+                v-model="naoResideNoBrasil"
                 inset
                 label="Não resido no Brasil"
               />
             </v-col>
 
-            <v-col cols="12" sm="4">
+            <v-col cols="12" sm="4" v-show="!naoResideNoBrasil">
               <v-select v-model="dataResponse.sg_uf_servidor_estadual" :error-messages="errorData.sg_uf_servidor_estadual"
                         :items="sgUfServidorEstadual" outlined label="De qual UF?" item-text="tx_nome_uf" item-value="sg_uf" @change="getMunicipio()"/>
             </v-col>
 
-            <v-col cols="12" sm="4">
+            <v-col cols="12" sm="4" v-show="!naoResideNoBrasil">
               <v-select v-model="dataResponse.sg_uf_servidor_estadual" :error-messages="errorData.sg_uf_servidor_estadual"
                         :items="sgUfServidorEstadual" outlined label="De qual Municipio?" item-text="tx_nome_uf" item-value="sg_uf" @change="getMunicipio()"/>
             </v-col>
 
-            <v-col cols="12" sm="4">
+            <v-col cols="12" sm="4" v-show="naoResideNoBrasil">
               <v-select v-model="dataResponse.sg_uf_servidor_estadual" :error-messages="errorData.sg_uf_servidor_estadual"
                         :items="sgUfServidorEstadual" outlined label="Qual país?" item-text="tx_nome_uf" item-value="sg_uf" @change="getMunicipio()"/>
             </v-col>
@@ -207,7 +216,16 @@ export default {
     sgUfServidorEstadual: [],
     idMunicipioServidorMunicipal: [],
     idOrgaoServidor: [],
-    resideNoBrasil: false,
+    naoResideNoBrasil: false,
+    loadingOrgaos: false,
+    loadingUf: false,
+    loadingMunicipio: false,
+    showFields: {
+      tp_esfera_servidor_militar: false,
+      id_orgao_servidor: false,
+      sg_uf_servidor_estadual: false,
+      id_municipio_servidor_municipal: false
+    }
   }),
   async mounted() {
     // await this.getUf();
@@ -215,16 +233,90 @@ export default {
   },
   methods: {
     async getUf() {
+      this.loadingUf = true;
       const response = await get('/uf?pagination=false');
       this.sgUfServidorEstadual = response.data.data;
+      this.loadingUf = false;
     },
     async getMunicipio() {
+      this.loadingMunicipio = true;
       const response = await get(`/municipio?pagination=false&search=sg_uf:${this.dataResponse.sg_uf_servidor_estadual}`);
       this.idMunicipioServidorMunicipal = response.data.data;
+      this.loadingMunicipio = false;
     },
-    async getOrgao() {
-      const response = await get('/orgao?search=bl_status_orgao:true&pagination=false&orderBy=tx_nome_orgao');
+    async getOrgao(params) {
+      this.loadingOrgaos = true;
+      const response = await get(`/orgao?search=bl_status_orgao:true${params}&pagination=false&orderBy=tx_nome_orgao&searchJoin=and`);
       this.idOrgaoServidor = response.data.data;
+      this.loadingOrgaos = false;
+    },
+    
+    
+    
+    tratarServidorPublico() {
+      const tipoPoder = this.dataResponse.tp_poder_execut_legisl_judic;
+      this.showFields.tp_esfera_servidor_militar =  true;
+      
+      switch (tipoPoder) {
+        case tipoPoder == 'E':
+          this.tratarEsferaPoderExecutivoServidor();
+          break;
+        case tipoPoder == 'J':
+          break;
+        case tipoPoder == 'L':
+          break;
+        case tipoPoder == 'O':
+          break;
+        default:
+          break
+      }
+
+      // if (tipoPoder == 'E') {
+      //   this.tratarEsferaPoderExecutivoServidor();
+      // } else if (tipoPoder == 'J') {
+      //   // Remove a opcao de esfera municipal para o poder judiciario
+      //   // $("#tp_esfera_servidor_militar option[value='3']").remove();
+      //   // tratarEsferaPoderJudiciarioServidor();
+      // } else if (tipoPoder == 'L') {
+      //   // tratarEsferaPoderLegislativoServidor();
+      // } else if (tipoPoder == 'O') {
+      //   // tratarEsferaPoderAutonomosServidor();
+      // }
+    },
+    tratarEsferaPoderExecutivoServidor() {
+      const tipoPoder = this.dataResponse.tp_poder_execut_legisl_judic;
+      const tipoEsfera = this.dataResponse.tp_esfera_servidor_militar;
+      
+      if (tipoPoder === 'E') {
+        
+        if (tipoEsfera === 'F') {
+          this.getOrgao(';id_esfera:1;id_vinculo:4');
+          this.showFields.id_orgao_servidor = true;
+        }
+
+        if (tipoEsfera === 'E' || tipoEsfera === 'M') {
+          this.showFields.id_orgao_servidor = false;
+          this.showFields.sg_uf_servidor_estadual = true;
+          this.getUf();
+        }
+        
+        //TODO - deixei esse if, para se caso durante o desenvolvimento precisar de regras especificas...
+        // if (tipoEsfera === 'M') {
+        //   this.showFields.id_orgao_servidor = false;
+        //   this.showFields.sg_uf_servidor_estadual = true;
+        //   this.getUf();
+        // }
+        
+      }
+    },
+    verificarRegrasParaUf() {
+      // const tipoPoder = this.dataResponse.tp_poder_execut_legisl_judic;
+      const tipoEsfera = this.dataResponse.tp_esfera_servidor_militar;
+
+      if (tipoEsfera === 'M') {
+        this.showFields.id_municipio_servidor_municipal = true;
+        this.getMunicipio()
+      }
     }
   }
 }
