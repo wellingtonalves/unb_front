59b7dfb19a82b8354a093bc93e5de7ef53f5ea8b
Alex Alexandre <alex.alexandre@redes.unb.br>
2021-03-15 17:42:15 -0300

    [21149][UPD] - corrigindo nome de permissões

--- src/views/ofertas/Exclusividade/GerenciarExclusividade.vue
+++ src/views/ofertas/Exclusividade/GerenciarExclusividade.vue
@@ -20,17 +20,17 @@
               Voltar
             </v-btn>
 
-            <v-btn outlined color="primary" v-show="$permissao('EXCLUSIVIDADE_EDITAR')"
+            <v-btn outlined color="primary" v-show="$permissao('EXCLUSIVIDADE_OFERTA_EDITAR')"
                @click="$router.push(`/ofertas/${oferta.id_oferta}/gerenciar-exclusividade/${oferta.exclusividade.id_exclusividade_oferta}/create`)" class="ma-1">
               Adicionar Valor
             </v-btn>
 
-            <v-btn outlined color="primary" v-show="$permissao('EXCLUSIVIDADE_EDITAR')"
+            <v-btn outlined color="primary" v-show="$permissao('EXCLUSIVIDADE_OFERTA_EDITAR')"
                    @click="$router.push(`/ofertas/${oferta.id_oferta}/exclusividade/${oferta.exclusividade.id_exclusividade_oferta}/edit`)" class="ma-1">
               Editar
             </v-btn>
 
-            <v-btn outlined color="error" v-show="$permissao('EXCLUSIVIDADE_EXCLUIR')"
+            <v-btn outlined color="error" v-show="$permissao('EXCLUSIVIDADE_OFERTA_EXCLUIR')"
                    @click="excluir()" class="ma-1">
               Remover exclusividade
             </v-btn>
